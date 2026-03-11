b86dd4842bbf78934298ab7f20a73571f1f5bfaa
Abilmansur <flyxnyfayfryka@gmail.com>
2026-03-12 02:02:59 +0800

    fig bugs3

--- states.py
+++ states.py
@@ -3,6 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class OrderStates(StatesGroup):
     idle = State()
+    split_detected = State()
     text_received = State()
     awaiting_payment = State()
     pending_approval = State()
